389178764ffbd8badcde1e62e240efd139d20754
Fazhi Tu <fazhi.tu@gmail.com>
2022-07-31 14:14:28 -0700

    feat: dependencies in fastapi endpoints

--- backend/app/api/routes/cleanings.py
+++ backend/app/api/routes/cleanings.py
@@ -1,5 +1,11 @@
 from typing import List
-from fastapi import APIRouter
+from urllib import response
+from fastapi import APIRouter, Body, Depends
+from starlette.status import HTTP_201_CREATED
+
+from app.models.cleaning import CleaningCreate, CleaningPublic
+from app.db.repositories.cleanings import CleaningRepository
+from app.api.dependencies.database import get_repository
 
 router = APIRouter()
 
@@ -10,3 +16,11 @@ async def get_all_cleanings() -> List[dict]:
         {"id": 2, "name": "Someone else's house", "cleaning_type": "spot_clean", "price_per_hour": 19.99}
     ]
     return cleanings
+
+@router.post("/", response_model=CleaningPublic, name="cleanings:create-cleaning", status_code=HTTP_201_CREATED)
+async def create_new_cleaning(
+    new_cleaning: CleaningCreate = Body(..., embed=True),
+    cleanings_repo: CleaningRepository = Depends(get_repository(CleaningRepository))
+) -> CleaningPublic:
+    created_cleaning = await cleanings_repo.create_cleaning(new_cleaning=new_cleaning)
+    return created_cleaning
