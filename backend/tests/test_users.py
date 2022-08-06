1d188137c382be13bf41e1722ed1058509c20f5d
Fazhi Tu <fazhi.tu@gmail.com>
2022-08-06 14:40:56 -0700

    feat: configuring our auth service

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI
 from databases import Database
 from app.db.repositories.users import UsersRepository
 
+from app.services import auth_service
+
 from starlette.status import (
     HTTP_200_OK,
     HTTP_201_CREATED,
@@ -70,3 +72,28 @@ class TestUserRegistration:
 
         res = await client.post(app.url_path_for("users:register-new-user"), json={"new_user": new_user})
         assert res.status_code == status_code
+    
+    async def test_users_saved_password_is_hashed_and_has_salt(
+        self, 
+        app: FastAPI,
+        client: AsyncClient,
+        db: Database,
+    ) -> None:
+        user_repo = UsersRepository(db)
+        new_user = {"email": "beyonce@knowles.io", "username": "queenbey", "password": "destinyschild"}
+
+        # send post request to create user and ensure it is successful
+        res = await client.post(app.url_path_for("users:register-new-user"), json={"new_user": new_user})
+        assert res.status_code == HTTP_201_CREATED
+
+        # ensure that the users password is hashed in the db
+        # and that we can verify it using our auth service
+        user_in_db = await user_repo.get_user_by_email(email=new_user["email"])
+        assert user_in_db is not None
+        assert user_in_db.salt is not None and user_in_db.salt != "123"
+        assert user_in_db.password != new_user["password"]
+        assert auth_service.verify_password(
+            password=new_user["password"], 
+            salt=user_in_db.salt, 
+            hashed_pw=user_in_db.password
+        )
