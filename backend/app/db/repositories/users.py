05c847d279bce186884b8a24918bd8e1a1b8aa21
Fazhi Tu <fazhi.tu@gmail.com>
2022-08-07 16:17:52 -0700

    feat: setting up user profiles in fastapi

--- backend/app/db/repositories/users.py
+++ backend/app/db/repositories/users.py
@@ -5,9 +5,10 @@ from fastapi import HTTPException, status
 from databases import Database
 
 from app.db.repositories.base import BaseRepository
-from app.models.user import UserCreate, UserUpdate, UserInDB
+from app.models.user import UserCreate, UserUpdate, UserInDB, UserPublic
 from app.services import auth_service
-
+from app.db.repositories.profiles import ProfilesRepository
+from app.models.profile import ProfileCreate, ProfilePublic
 
 GET_USER_BY_EMAIL_QUERY = """
     SELECT id, username, email, email_verified, password, salt, is_active, is_superuser, created_at, updated_at
@@ -32,22 +33,23 @@ class UsersRepository(BaseRepository):
     def __init__(self, db: Database) -> None:
         super().__init__(db)
         self.auth_service = auth_service
+        self.profiles_repo = ProfilesRepository(db)
 
-    async def get_user_by_email(self, *, email: EmailStr) -> UserInDB:
+    async def get_user_by_email(self, *, email: EmailStr, populate: bool = True) -> UserInDB:
         user_record = await self.db.fetch_one(query=GET_USER_BY_EMAIL_QUERY, values={"email": email})
+        if user_record:
+            user = UserInDB(**user_record)
+            if populate:
+                return await self.populate_user(user=user)
+            return user
 
-        if not user_record:
-            return None
-
-        return UserInDB(**user_record)
-
-    async def get_user_by_username(self, *, username: str) -> UserInDB:
+    async def get_user_by_username(self, *, username: str, populate: bool = True) -> UserInDB:
         user_record = await self.db.fetch_one(query=GET_USER_BY_USERNAME_QUERY, values={"username": username})
-
-        if not user_record:
-            return None
-
-        return UserInDB(**user_record)
+        if user_record:
+            user = UserInDB(**user_record)
+            if populate:
+                return await self.populate_user(user=user)
+            return user
 
     async def register_new_user(self, *, new_user: UserCreate) -> UserInDB:
         # make sure email isn't already taken
@@ -68,11 +70,16 @@ class UsersRepository(BaseRepository):
         new_user_params = new_user.copy(update=user_password_update.dict())
         created_user = await self.db.fetch_one(query=REGISTER_NEW_USER_QUERY, values=new_user_params.dict())
 
-        return UserInDB(**created_user)
+        # create profile for new user
+        await self.profiles_repo.create_profile_for_user(
+            profile_create=ProfileCreate(user_id=created_user["id"])
+        )
+
+        return await self.populate_user(user=UserInDB(**created_user))
 
     async def authenticate_user(self, *, email: EmailStr, password: str) -> Optional[UserInDB]:
         # make sure user exists in db
-        user = await self.get_user_by_email(email=email)
+        user = await self.get_user_by_email(email=email, populate=False)
         if not user:
             return None
         # if submitted password doesn't match
@@ -80,4 +87,13 @@ class UsersRepository(BaseRepository):
             return None
         
         return user
+
+    async def populate_user(self, *, user: UserInDB) -> UserInDB:
+        return UserPublic(
+            # unpack the user in db dict into the UserPublic model
+            # which will remove "password" and "salt"
+            **user.dict(),
+            # fetch the user's profile from the profiles repo
+            profile=await self.profiles_repo.get_profile_by_user_id(user_id=user.id)
+        )
 
