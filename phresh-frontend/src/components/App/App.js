f8ccfeb0d8ecb363f48633b24a40f4c50f5480e0
Fazhi Tu <fazhi.tu@gmail.com>
2022-08-13 09:11:03 -0700

    feat: getting familiar with elastic ui and styled components

---
+++ phresh-frontend/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Layout } from "../../components"
+import { EuiText } from "@elastic/eui"
+
+export default function App() {
+  return (
+    <Layout>
+      <EuiText>
+        <h1>Phresh</h1>
+      </EuiText>
+    </Layout>
+  )
+}
